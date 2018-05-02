362e2b8a92f85a8ab13ff9238a8b27c3c635064a
RikMachado <ricardofvmachado96@gmail.com>
2018-05-02 18:47:01 +0100

    Update Viatura_OLD.java

--- Core/Viatura_OLD.java
+++ Core/Viatura_OLD.java
@@ -3,104 +3,23 @@ package projeto.pp;
 import java.util.*;
 import java.lang.*;
 
-class Viatura extends Thread{
+class Viatura {
 
 //Atributos
     int tipoAutocarro,num_max_pessoas,num_atual_pessoas,ID;
-    int cidadeOrigem, cidadeDestino, cidadeInt;
-    Boolean Avariado = false, SentidoSul;
-    int percurso;
-    String cidade;
+    //int cidadeOrigem, cidadeDestino, cidadeInt;
+    Boolean Avariado = false, //SentidoSul;
+    //int percurso;
+    //String cidade;
     
-    public Viatura(String cidadeOrigem, String cidadeDestino, boolean SentidoSul){
-       
-        this.SentidoSul = SentidoSul; 
-        DefinirCidade(cidadeOrigem);
-        this.cidadeOrigem = cidadeInt;
-        DefinirCidade(cidadeDestino);  
-        this.cidadeDestino = cidadeInt;
-       
-        percurso =  Math.abs(this.cidadeDestino - this.cidadeOrigem);
-      
-        if (this.SentidoSul == true) { 
-            switch (cidadeOrigem){
-                case "Cascais": this.cidadeOrigem = 100;
-                    break;
-                case "Lisboa": this.cidadeOrigem = 75;
-                    break;
-                case "Coimbra": this.cidadeOrigem = 50;
-                    break;
-                case "Porto": this.cidadeOrigem = 20;
-                    break;
-                case "Braga": this.cidadeOrigem = 0;
-                    break;
-            } 
-        }
+    
     }
-    
-@Override
-   public void run(){
-      
-      //Verificar Sentido e Calcular Percurso 
-      int i = cidadeOrigem;
-      for (int x=i; x<=(i+percurso) ;x++)
-      {
-        if (SentidoSul == false){
-             
-            switch (x){
-                case 0: System.out.println("Cascais");
-                    break;
-                case 20: System.out.println("Lisboa");
-                    break; 
-                case 50: System.out.println("Coimbra");
-                    break;
-                case 75: System.out.println("Porto");
-                    break;
-                case 100: System.out.println("Braga");
-                    break;
-            }
-        }
-        else{
-            switch (x){
-            case 0: System.out.println("Braga");
-                break;
-            case 20: System.out.println("Porto");
-                break;
-            case 50: System.out.println("Coimbra");
-                break;
-            case 75: System.out.println("Lisboa");
-                break;
-            case 100: System.out.println("Cascais");
-                break;
-            }
-        
-        }   
-        try{
-            Thread.sleep(20);}
-        catch(Exception e){}
-      
-      }
-      //Avaria
-   }
-   
-   
-   public int DefinirCidade(String cidade){
-       
-       switch (cidade){
-            case "Cascais": cidadeInt = 0;
-                break;
-            case "Lisboa": cidadeInt = 20;
-                break;
-            case "Coimbra": cidadeInt = 50;
-                break;
-            case "Porto": cidadeInt = 75;
-                break;
-            case "Braga": cidadeInt = 100;
-                break;
-      } 
-      return cidadeInt;
-   }
 }
+    
+
+   
+   
+   
 
 class Convencional extends Viatura{
     
