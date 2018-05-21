c3538474adfd9a4fecb8807e906e6031f458ac46
RikMachado <ricardofvmachado96@gmail.com>
2018-05-21 17:31:14 +0100

    Add files via upload
    exception added

---
+++ Core/Viaturas.java
@@ -0,0 +1,172 @@
+package projeto.pp;
+
+import java.util.*;
+import java.lang.*;
+
+class testarPessoas extends Exception {
+
+    public testarPessoas() {
+        super();
+    }
+
+    public testarPessoas(String msg) {
+        super(msg);
+    }
+}
+
+public class Viaturas {
+
+    //Atributos           
+    int tipoAutocarro, num_max_pessoas = 51, num_atual_pessoas, ID, pessoasInicial;
+    int velocidade = 70;
+    int velocidadeBase = 70;
+    double velocidadeDouble = 70;
+    double velocidadeBaseDouble = 70;
+
+    String tipoViatura = "debug";
+
+    boolean Avariado = false; //SentidoSul;
+
+    public Viaturas(int ID, int pessoasInicial) {
+
+        this.ID = ID;
+        this.pessoasInicial = pessoasInicial;
+    }
+
+    public Viaturas() {
+    }
+
+    public String getID() {
+
+        return Integer.toString(ID);
+    }
+
+    public Boolean getAvaria() {
+
+        return this.Avariado;
+    }
+
+    public int getPessoasInicial() {
+
+        return this.pessoasInicial;
+    }
+
+    public String getTipoAutocarro() {
+
+        return this.tipoViatura;
+    }
+
+    public void setAvaria(Boolean estado) {
+
+        this.Avariado = estado;
+    }
+
+    public int getMaxPessoas() {
+
+        return this.num_max_pessoas;
+    }
+
+    public int getVelocidade() {
+        
+        double tempCalc = ((this.velocidadeBaseDouble / this.velocidadeDouble) * 1000) + 400;
+        int returnValue = (int) tempCalc;
+
+        return returnValue;
+    }
+
+    public double multTempo() {
+
+        int returnValue = (int) ((this.velocidadeBaseDouble / this.velocidadeDouble) + 2);
+        return returnValue;
+    }
+
+    void excPessMax(int pessoas, int pessoasMax) throws testarPessoas {
+        
+        if (pessoas > pessoasMax) {
+            throw new testarPessoas();
+        }
+    }
+
+}
+
+class Convencional extends Viaturas {
+
+    public Convencional(int ID, int pessoasInicial) {
+        
+        this.ID = ID;
+
+        try {
+            this.pessoasInicial = pessoasInicial;
+            excPessMax(this.num_max_pessoas, this.pessoasInicial);
+        } catch (testarPessoas tP) {
+            tP.printStackTrace();
+        }
+
+        this.num_max_pessoas = 51;
+        this.tipoViatura = "Convencional";
+        this.velocidade = 80;
+        this.velocidadeDouble = this.velocidade;
+    }
+
+}
+
+class MiniBus extends Viaturas {
+
+    public MiniBus(int ID, int pessoasInicial) {
+        
+        this.ID = ID;
+
+        try {
+            this.pessoasInicial = pessoasInicial;
+            excPessMax(this.num_max_pessoas, this.pessoasInicial);
+        } catch (testarPessoas tP) {
+            tP.printStackTrace();
+        }
+
+        this.num_max_pessoas = 24;
+        this.tipoViatura = "MiniBus";
+        this.velocidade = 120;
+        this.velocidadeDouble = this.velocidade;
+    }
+
+}
+
+class LongDrive extends Viaturas {
+
+    public LongDrive(int ID, int pessoasInicial) {
+        
+        this.ID = ID;
+
+        try {
+            this.pessoasInicial = pessoasInicial;
+            excPessMax(this.num_max_pessoas, this.pessoasInicial);
+        } catch (testarPessoas tP) {
+            tP.printStackTrace();
+        }
+        this.num_max_pessoas = 59;
+        this.tipoViatura = "LongDrive";
+        this.velocidade = 50;
+        this.velocidadeDouble = this.velocidade;
+    }
+}
+
+class Expresso extends Viaturas {
+
+    public Expresso(int ID, int pessoasInicial) {
+        
+        this.ID = ID;
+        
+        try {
+            this.pessoasInicial = pessoasInicial;
+            excPessMax(this.num_max_pessoas, this.pessoasInicial);
+        } catch (testarPessoas tP) {
+            tP.printStackTrace();
+        }
+        
+        this.num_max_pessoas = 69;
+        this.tipoViatura = "Expresso";
+        this.velocidade = 100;
+        this.velocidadeDouble = this.velocidade;
+    }
+
+}
